e99aee08735529925b386a2dc3fce27b4e68f3ad
Andy Lassiter <andrewl@wustl.edu>
2023-10-23 15:46:38 -0500

    Try to render subject overview graphs in dash app

--- dash/subject-demographics.py
+++ dash/subject-demographics.py
@@ -16,9 +16,13 @@ app = Dash(
 )
 
 # XNAT setup
-project_id = os.environ['XNAT_ITEM_ID']
+xnat_host = os.getenv('XNAT_HOST')
+xnat_user = os.getenv('XNAT_USER')
+xnat_password = os.getenv('XNAT_PASS')
 
-connection = xnat.connect()
+project_id = os.getenv('XNAT_ITEM_ID')
+
+connection = xnat.connect(xnat_host, user=xnat_user, password=xnat_password)
 project = connection.projects[project_id]
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
@@ -91,53 +95,52 @@ def render_home():
         html.P(f"Experiments: {len(project.experiments)}", className="lead"),
     ])
 
-# def render_subjects():
-#     return html.Div([
-#         html.P("Subject Overview", className="lead"),
-#         dcc.Graph(id='subject-age-distribution', figure=subject_age_distribution()),
-#     ])
+def render_subjects():
+    return html.Div([
+        html.P("Subject Overview", className="lead"),
+        dcc.Graph(id='subject-age-distribution', figure=subject_age_distribution()),
+    ])
 
-# # Cache for subject data
-# subject_data_cache = None
+# Cache for subject data
+subject_data_cache = None
 
-# # Compile subject data or return cached data
-# def get_subject_data():
-#     global subject_data_cache
+# Compile subject data or return cached data
+def get_subject_data():
+    global subject_data_cache
     
-#     if subject_data_cache is not None:
-#         return subject_data_cache
+    if subject_data_cache is not None:
+        return subject_data_cache
     
-#     subject_data = {
-#         'id': [],
-#         'gender': [],
-#         'age': []
-#     }
+    subject_data = {
+        'id': [],
+        'gender': [],
+        'age': []
+    }
 
-#     for subject in project.subjects.values():
-#         subject_data['id'].append(subject.label)
-#         subject_data['gender'].append(subject.demographics.gender)
-#         subject_data['age'].append(subject.demographics.age)
+    for subject in project.subjects.values():
+        subject_data['id'].append(subject.label)
+        subject_data['gender'].append(subject.demographics.gender)
+        subject_data['age'].append(subject.demographics.age)
         
-#     df = pd.DataFrame(subject_data)
+    df = pd.DataFrame(subject_data)
     
-#     subject_data_cache = df
+    subject_data_cache = df
 
-#     return df
+    return df
 
-# def subject_age_distribution():
-#     ages = get_subject_data()['age']
+def subject_age_distribution():
+    ages = get_subject_data()['age']
 
-#     fig = px.histogram(ages, nbins=20)
-#     fig.update_layout(
-#         title_text='Age Distribution',
-#         xaxis_title_text='Age',
-#         yaxis_title_text='Count',
-#         bargap=0.2,
-#         bargroupgap=0.1
-#     )
+    fig = px.histogram(ages, nbins=20)
+    fig.update_layout(
+        title_text='Age Distribution',
+        xaxis_title_text='Age',
+        yaxis_title_text='Count',
+        bargap=0.2,
+        bargroupgap=0.1
+    )
 
-#     return fig
-
+    return fig
 
 if __name__ == "__main__":
     app.run_server(port=8050, host='0.0.0.0', debug=True)
