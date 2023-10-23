fecb188a565664b5fe8234945b1062ac2254a7ed
Andy Lassiter <andrewl@wustl.edu>
2023-10-23 13:53:46 -0500

    Try to add a histogram plot to dash subject demographics app

--- dash/subject-demographics.py
+++ dash/subject-demographics.py
@@ -1,6 +1,7 @@
 import dash_bootstrap_components as dbc
 import os
 import xnat
+import plotly.express as px
 from dash import Dash, html, dcc, Output, Input
 
 # Dash setup
@@ -40,13 +41,13 @@ CONTENT_STYLE = {
 
 sidebar = html.Div(
     [
-        html.H2("XNAT Dash App", className="display-7"),
+        html.H3("XNAT Dash App", className="display-7"),
         html.Hr(),
         html.P(f"Project: {project_id}", className="lead"),
         dbc.Nav(
             [
                 dbc.NavLink("Project Overview", href=jupyterhub_base_url, active="exact"),
-                dbc.NavLink("Page 1", href=f"{jupyterhub_base_url}page-1", active="exact"),
+                dbc.NavLink("Subject Overview", href=f"{jupyterhub_base_url}subject-overview", active="exact"),
                 dbc.NavLink("Page 2", href=f"{jupyterhub_base_url}page-2", active="exact"),
             ],
             vertical=True,
@@ -68,7 +69,7 @@ def render_page_content(pathname):
 
     if pathname == "/":
         return render_home()
-    elif pathname == "/page-1":
+    elif pathname == "/subject-overview":
         return html.P("This is the content of page 1. Yay!")
     elif pathname == "/page-2":
         return html.P("Oh cool, this is page 2!")
@@ -89,5 +90,53 @@ def render_home():
         html.P(f"Experiments: {len(project.experiments)}", className="lead"),
     ])
 
+def render_subjects():
+    return html.Div([
+        html.P("Subject Overview", className="lead"),
+        dcc.Graph(id='subject-age-distribution', figure=subject_age_distribution()),
+    ])
+
+# Cache for subject data
+subject_data_cache = None
+
+# Compile subject data or return cached data
+def get_subject_data():
+    global subject_data_cache
+    
+    if subject_data_cache is not None:
+        return subject_data_cache
+    
+    subject_data = {
+        'id': [],
+        'gender': [],
+        'age': []
+    }
+
+    for subject in project.subjects.values():
+        subject_data['id'].append(subject.label)
+        subject_data['gender'].append(subject.demographics.gender)
+        subject_data['age'].append(subject.demographics.age)
+        
+    df = pd.DataFrame(subject_data)
+    
+    subject_data_cache = df
+
+    return df
+
+def subject_age_distribution():
+    ages = get_subject_data()['age']
+
+    fig = px.histogram(ages, nbins=20)
+    fig.update_layout(
+        title_text='Age Distribution',
+        xaxis_title_text='Age',
+        yaxis_title_text='Count',
+        bargap=0.2,
+        bargroupgap=0.1
+    )
+
+    return fig
+
+
 if __name__ == "__main__":
-    app.run_server(port=8050, host='0.0.0.0')
+    app.run_server(port=8050, host='0.0.0.0', debug=True)
