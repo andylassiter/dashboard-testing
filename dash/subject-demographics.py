b71d82dfcb5e035d60fd3f063a2ab49491e82d9b
Andy Lassiter <andrewl@wustl.edu>
2023-10-23 16:11:37 -0500

    Add logging statements to dash subject demographics app

--- dash/subject-demographics.py
+++ dash/subject-demographics.py
@@ -5,9 +5,17 @@ import xnat
 import pandas as pd
 import plotly.express as px
 
+# Logging to stdout
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 # Dash setup
 user = os.getenv('JUPYTERHUB_USER')
 jupyterhub_base_url = os.getenv('JUPYTERHUB_SERVICE_PREFIX', f"/jupyterhub/user/{user}/")
+
+logging.debug(f"JupyterHub base URL: {jupyterhub_base_url}")
+logging.debug(f"Starting Dash app")
 
 app = Dash(
     __name__,
@@ -15,12 +23,19 @@ app = Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP]
 )
 
+logging.debug(f"Dash app created")
+
 # XNAT setup
 xnat_host = os.getenv('XNAT_HOST')
 xnat_user = os.getenv('XNAT_USER')
 xnat_password = os.getenv('XNAT_PASS')
 
 project_id = os.getenv('XNAT_ITEM_ID')
+
+logging.debug(f"XNAT host: {xnat_host}")
+logging.debug(f"XNAT user: {xnat_user}")
+logging.debug(f"XNAT password: {xnat_password}")
+logging.debug(f"XNAT project ID: {project_id}")
 
 connection = xnat.connect(xnat_host, user=xnat_user, password=xnat_password)
 project = connection.projects[project_id]
@@ -105,52 +120,52 @@ def render_iris():
     ])
 
 
-def render_subjects():
-    return html.Div([
-        html.P("Subject Overview", className="lead"),
-        dcc.Graph(id='subject-age-distribution', figure=subject_age_distribution()),
-    ])
+# def render_subjects():
+#     return html.Div([
+#         html.P("Subject Overview", className="lead"),
+#         dcc.Graph(id='subject-age-distribution', figure=subject_age_distribution()),
+#     ])
 
-# Cache for subject data
-subject_data_cache = None
+# # Cache for subject data
+# subject_data_cache = None
 
-# Compile subject data or return cached data
-def get_subject_data():
-    global subject_data_cache
+# # Compile subject data or return cached data
+# def get_subject_data():
+#     global subject_data_cache
     
-    if subject_data_cache is not None:
-        return subject_data_cache
+#     if subject_data_cache is not None:
+#         return subject_data_cache
     
-    subject_data = {
-        'id': [],
-        'gender': [],
-        'age': []
-    }
+#     subject_data = {
+#         'id': [],
+#         'gender': [],
+#         'age': []
+#     }
 
-    for subject in project.subjects.values():
-        subject_data['id'].append(subject.label)
-        subject_data['gender'].append(subject.demographics.gender)
-        subject_data['age'].append(subject.demographics.age)
+#     for subject in project.subjects.values():
+#         subject_data['id'].append(subject.label)
+#         subject_data['gender'].append(subject.demographics.gender)
+#         subject_data['age'].append(subject.demographics.age)
         
-    df = pd.DataFrame(subject_data)
+#     df = pd.DataFrame(subject_data)
     
-    subject_data_cache = df
+#     subject_data_cache = df
 
-    return df
+#     return df
 
-def subject_age_distribution():
-    ages = get_subject_data()['age']
+# def subject_age_distribution():
+#     ages = get_subject_data()['age']
 
-    fig = px.histogram(ages, nbins=20)
-    fig.update_layout(
-        title_text='Age Distribution',
-        xaxis_title_text='Age',
-        yaxis_title_text='Count',
-        bargap=0.2,
-        bargroupgap=0.1
-    )
+#     fig = px.histogram(ages, nbins=20)
+#     fig.update_layout(
+#         title_text='Age Distribution',
+#         xaxis_title_text='Age',
+#         yaxis_title_text='Count',
+#         bargap=0.2,
+#         bargroupgap=0.1
+#     )
 
-    return fig
+#     return fig
 
 if __name__ == "__main__":
     app.run_server(port=8050, host='0.0.0.0', debug=True)
