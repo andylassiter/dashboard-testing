44afbd58e34a7da1e641d073492e59e83214e6fc
Andy Lassiter <andrewl@wustl.edu>
2023-10-24 17:23:06 -0500

    Update panel with age histogram

--- panel/subject-demographics.py
+++ panel/subject-demographics.py
@@ -1,9 +1,18 @@
 import panel as pn
 import hvplot.pandas
+
 import pandas as pd
 import numpy as np
 import xnat
 import os
+
+# For local testing
+os.environ['JUPYTERHUB_USER'] = 'admin'
+os.environ['JUPYTERHUB_SERVICE_PREFIX'] = '/'
+os.environ['XNAT_HOST'] = 'http://localhost'
+os.environ['XNAT_USER'] = 'admin'
+os.environ['XNAT_PASS'] = 'admin'
+os.environ['XNAT_ITEM_ID'] = 'C4KC-KiTS'
 
 # XNAT setup
 xnat_host = os.getenv('XNAT_HOST')
@@ -46,13 +55,23 @@ def get_subject_data():
 # Panel setup
 pn.extension(design='material')
 
-csv_file = ("https://raw.githubusercontent.com/holoviz/panel/main/examples/assets/occupancy.csv")
-data = pd.read_csv(csv_file, parse_dates=["date"], index_col="date")
-
 df = get_subject_data()
 df_pane = pn.pane.DataFrame(df, sizing_mode="stretch_both", max_height=300)
 
-first_app = pn.Column(df_pane)
+histogram_age = df.hvplot.hist('age', bins=15, title='Age Distribution', height=300)
+histogram_age.opts(xlabel='Age', ylabel='Count')
+histogram_age.opts(width=500, height=300)
 
-first_app.servable()
-first_app
+template = pn.template.BootstrapTemplate(
+    title=f"XNAT Panel App",
+    busy_indicator=pn.indicators.BooleanStatus(value=False)
+)
+
+template.main.append(
+    pn.Row(
+        pn.Column(df_pane),
+        pn.Column(histogram_age)
+    )
+)
+
+template.servable()
