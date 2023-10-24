ceaddb154ecd145fa1f348b5faa1b2ea6a7ca6f1
Andy Lassiter <andrewl@wustl.edu>
2023-10-24 16:53:07 -0500

    Update panel app to have max size of data frame

--- panel/subject-demographics.py
+++ panel/subject-demographics.py
@@ -50,7 +50,7 @@ csv_file = ("https://raw.githubusercontent.com/holoviz/panel/main/examples/asset
 data = pd.read_csv(csv_file, parse_dates=["date"], index_col="date")
 
 df = get_subject_data()
-df_pane = pn.pane.DataFrame(df, max_rows=15)
+df_pane = pn.pane.DataFrame(df, sizing_mode="stretch_both", max_height=300)
 
 first_app = pn.Column(df_pane)
 
