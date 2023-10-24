30585471454cd848db3a8147cd55f4e322f903bd
Andy Lassiter <andrewl@wustl.edu>
2023-10-24 15:42:13 -0500

    Hmmm dash..

--- dash/subject-demographics-2.py
+++ dash/subject-demographics-2.py
@@ -84,15 +84,7 @@ def render_page_content(pathname):
     pathname = pathname.replace(jupyterhub_base_url, '/')
 
     if pathname == "/":
-        return dbc.Container([
-        dbc.Row(
-            [
-                dbc.Col(html.Div("One of three columns")),
-                dbc.Col(html.Div("One of three columns")),
-                dbc.Col(html.Div("One of three columns")),
-            ]
-        ),
-    ])
+        return html.P("This is the content of the home page!")
     elif pathname == "/page-1":
         return html.P("This is the content of page 1. Yay!")
     elif pathname == "/page-2":
