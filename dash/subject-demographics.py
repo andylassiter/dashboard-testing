ca16b6a6e4f75e7c3be970e4d2350cd9b1ced860
Andy Lassiter <andrewl@wustl.edu>
2023-11-02 13:48:33 -0500

    Trying to debug dash subject demographics

--- dash/subject-demographics.py
+++ dash/subject-demographics.py
@@ -1,17 +1,36 @@
-# Import packages
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import logging
+import os
+import xnat
+
+import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
-import dash_bootstrap_components as dbc
-import os
-import logging
-import xnat
+
+from dash import Dash, dash_table, dcc, html
+
+# For local testing
+# os.environ['JUPYTERHUB_USER'] = 'admin'
+# os.environ['JUPYTERHUB_SERVICE_PREFIX'] = '/'
+# os.environ['XNAT_HOST'] = 'http://localhost'
+# os.environ['XNAT_USER'] = 'admin'
+# os.environ['XNAT_PASS'] = 'admin'
+# os.environ['XNAT_ITEM_ID'] = 'UPENN-GBM'
 
 # Logging to a file
 logging.basicConfig(
     filename='dash.log',
     level=logging.INFO,
     format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+)
+
+# Dash setup
+user = os.getenv('JUPYTERHUB_USER')
+jupyterhub_base_url = os.getenv('JUPYTERHUB_SERVICE_PREFIX', f"/jupyterhub/user/{user}/")
+
+app = Dash(
+    __name__,
+    requests_pathname_prefix=f"{jupyterhub_base_url}",
+    external_stylesheets=[dbc.themes.CERULEAN]
 )
 
 # XNAT setup
@@ -24,9 +43,6 @@ project_id = os.getenv('XNAT_ITEM_ID')
 connection = xnat.connect(xnat_host, user=xnat_user, password=xnat_password)
 project = connection.projects[project_id]
 
-logging.info(f"Connected to XNAT project {project_id}")
-
-# Incorporate data
 # Compile subject data or return cached data
 def get_subject_data():
     subject_data = {
@@ -44,40 +60,49 @@ def get_subject_data():
 
     return df
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+df = get_subject_data()
 
-# Dash setup
-user = os.getenv('JUPYTERHUB_USER')
-jupyterhub_base_url = os.getenv('JUPYTERHUB_SERVICE_PREFIX', f"/jupyterhub/user/{user}/")
+def subject_age_distribution():
+    ages = get_subject_data()['age']
 
-logging.info(f"Creating Dash app named {__name__} with base URL {jupyterhub_base_url}")
+    fig = px.histogram(ages, nbins=20)
+    fig.update_layout(
+        title_text='Subject Ages',
+        xaxis_title_text='Age',
+        yaxis_title_text='Count',
+        bargap=0.2,
+        bargroupgap=0.1
+    )
 
-app = Dash(
-    __name__,
-    requests_pathname_prefix=f"{jupyterhub_base_url}",
-    external_stylesheets=[dbc.themes.CERULEAN]
-)
+    return fig
 
-logging.info(f"Created Dash app named {__name__} with base URL {jupyterhub_base_url}")
+def subject_gender_distribution():
+    genders = get_subject_data()['gender'].value_counts()
 
-# App layout
+    fig = px.pie(genders, values=genders.values, names=genders.index)
+    fig.update_layout(
+        title_text='Subject Genders',
+        showlegend=True
+    )
+
+    return fig
+
 app.layout = dbc.Container([
     dbc.Row([
-        html.Div(f"My First App with Data, Graph, and Controls for {project_id}", className="text-primary text-center fs-3")
-    ]),
-
-    dbc.Row([
         dbc.Col([
-            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
-        ], width=6),
-
-        dbc.Col([
-            dcc.Graph(figure={}, id='my-first-graph-final')
-        ], width=6),
-    ]),
-
+            html.H1(children=f"Dash App for {project.id}", style={'textAlign':'center'}),
+            html.H2(children=f"Subject Demographics for {project.id}"),
+            dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+            dbc.Row([
+                dbc.Col([
+                    dcc.Graph(id='subject-age-distribution', figure=subject_age_distribution())
+                ], width=6),
+                dbc.Col([
+                    dcc.Graph(id='subject-gender-distribution', figure=subject_gender_distribution())
+                ], width=6),
+            ])
+        ], width=8)
+    ], justify="center")
 ], fluid=True)
 
-# Run the app
-if __name__ == "__main__":
-    app.run_server(port=8050, host='0.0.0.0', debug=True)
+app.run_server(port=8050, host='0.0.0.0', debug=True)
